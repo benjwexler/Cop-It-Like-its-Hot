468f61d5e12123d3ddd536f5856434457f562b73
Ben Wexler <mixedywex@gmail.com>
2018-10-27 19:35:28 -0400

    need to deal with edge cases for what to display on screen

--- app/assets/javascripts/getPriceAndConfirm.js
+++ app/assets/javascripts/getPriceAndConfirm.js
@@ -1,4 +1,10 @@
+const convertToUsCurrency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  })
 
+  console.log(convertToUsCurrency.format(1000))
     
 
 
@@ -22,11 +28,17 @@ function firstIexCall(stockSymbol, quantity ) {
             fullPrice = data * quantity
             if(fullPrice < hiddenAccountBalance) { 
             fullPrice = fullPrice.toFixed(2)
-            document.getElementById("stockQuote").innerText = `$${fullPrice} is the cost for 6 shares of ${stockSymbol}`
-            document.getElementById("buyStock").style.display = "none";
-            document.getElementById("priceQuoteContainer").style.display = "flex";
+
+            document.getElementById("stockQuote").innerText = `${convertToUsCurrency.format(fullPrice)} is the cost for 6 shares of ${stockSymbol.toUpperCase()}`
+            // document.getElementById("buyStock").style.display = "none";
+            // document.getElementById("priceQuoteContainer").style.display = "flex";
             } else {
+                document.getElementById("stockQuote").innerText = ""
+                // same as function returnToGetPrice, but without .prevent default (will refactor the function with if statement if time)
+                getPriceForm.style.display = "block";
+                confirmTradeContainer.style.display = "none"
                 alert("Sorry, insufficient funds")
+                
             }
         }, error: function (data) {
             alert("Please check that you submitted a valid ticker symbol")
@@ -38,7 +50,9 @@ function firstIexCall(stockSymbol, quantity ) {
     $("#getPriceForm").submit(function (event) {
         event.preventDefault();
         stockSymbol = document.getElementById("stock_symbol").value
+        stockSymbol - stockSymbol.toUpperCase()
         quantity = document.getElementById("numberOfShares").value
+        quantity = quantity.toUpperCase()
         //   var data = $(this).serializeArray();
         let fullPrice
         console.log(quantity);
@@ -46,6 +60,9 @@ function firstIexCall(stockSymbol, quantity ) {
 
         if ((parseInt(quantity)) && (parseInt(quantity) > 0)) {
 
+            
+            // getPriceButton.click()
+            // cancelPurchaseBtn.click()
             // if (symbolsObj[`${stockSymbol.toUpperCase()}`] === 1) {
 
             
@@ -55,7 +72,14 @@ function firstIexCall(stockSymbol, quantity ) {
             //     alert("Fake Stock!")
             // }
         } else {
+            // same as function returnToGetPrice, but without .prevent default (will refactor the function with if statement if time)
+            getPriceForm.style.display = "block";
+            confirmTradeContainer.style.display = "none"
+            document.getElementById("stockQuote").innerText = ""
             alert("Please enter a valid quantity")
+            
+   
+            
         }
 
         
@@ -97,8 +121,8 @@ function returnToGetPrice(event){
     getPriceForm.style.display = "block";
     confirmTradeContainer.style.display = "none"
 }
-
 getPriceButton.addEventListener("click", hideGetPrice )
-cancelPurchaseBtn.addEventListener("click", returnToGetPrice)
+            cancelPurchaseBtn.addEventListener("click", returnToGetPrice)
+
 });
 
