7f802f616d14d6f84bf5bb383a9bb563992d1899
Ben Wexler <mixedywex@gmail.com>
2018-10-27 22:44:37 -0400

    changed migration to not have a column named 'type'

--- db/migrate/20181026204044_create_transactions.rb
+++ db/migrate/20181026204044_create_transactions.rb
@@ -2,7 +2,10 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.integer :user_id
-      t.string :type
+      # t.string :type
+      t.string :buy_or_sell
+      
+
       t.string :stock_symbol
       t.integer :shares
 
