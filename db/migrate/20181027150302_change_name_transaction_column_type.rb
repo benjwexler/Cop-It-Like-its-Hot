e8dc4485a38e868b5f4899773ff5dc310cdea0bf
Ben Wexler <mixedywex@gmail.com>
2018-10-27 22:39:15 -0400

    commited out a duplicate migration I thought didn't go through

--- db/migrate/20181027150302_change_name_transaction_column_type.rb
+++ db/migrate/20181027150302_change_name_transaction_column_type.rb
@@ -1,6 +1,6 @@
 class ChangeNameTransactionColumnType < ActiveRecord::Migration[5.2]
   def change
     # migration didn't work here, as I used incorrect syntax
-    rename_column :transactions, :type, :buy_or_sell
+    # rename_column :transactions, :type, :buy_or_sell
   end
 end
