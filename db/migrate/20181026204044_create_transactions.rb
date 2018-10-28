99377907f1423cf535bc9cd19700e885ca23825c
Ben Wexler <mixedywex@gmail.com>
2018-10-28 18:46:07 -0400

    nothing was really modified

--- db/migrate/20181026204044_create_transactions.rb
+++ db/migrate/20181026204044_create_transactions.rb
@@ -2,7 +2,6 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.integer :user_id
-      # t.string :type
       t.string :buy_or_sell
       
 
