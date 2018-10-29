6fecbe86211c9b144a3059e38c2e442681983a10
Ben Wexler <mixedywex@gmail.com>
2018-10-29 16:31:34 -0400

    removed uneeded comments from Rails

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,7 @@
 class TransactionsController < ApplicationController
   before_action :set_transaction, only: [:update, :destroy]
   before_action :authenticate_user!
-  # caches_page :index, :show  
 
-  # I seem to have set up the routing where there has been an issue twice with verifying the authenticy token and the app crashes. The following line of code:
-
-  # skip_before_action :verify_authenticity_token
-
-  # is supposed to fix the app from crashing, but as I understand this can put my app at risk for Cross-Site Request Forgery (CSRF) attacks.
-  
   
 
 
@@ -49,7 +42,7 @@ class TransactionsController < ApplicationController
    
     # I have hard coded the "buy_or_sell" type to be "BUY". If I add a sell feature this has to be changed
     @transaction = Transaction.create(:user_id => current_user_id, :buy_or_sell => "BUY", :stock_symbol => user_owned_stock, :shares => shares_to_be_added, :price_per_share => price_per_share)
-    # @transaction = Transaction.new(transaction_params)
+
     user_account_balance_before_transaction =   @user.account_balance
   
     total_price_of_transaction = price_per_share * shares_to_be_added
@@ -60,9 +53,9 @@ class TransactionsController < ApplicationController
     @holdings = @user.holdings
   #CREATES HOLDING   
     if @holdings.where(stock_symbol: [user_owned_stock]).length == 0
-      # @holding = Holding.new(holding_params)
+
       @holding = Holding.new(:user_id => current_user_id, :stock_symbol => user_owned_stock, :shares => shares_to_be_added)
-      # @holding = Holding.new(holding_params)
+
       respond_to do |format|
         if @holding.save
           format.html {  }
@@ -77,7 +70,7 @@ class TransactionsController < ApplicationController
      previous_shares_owned = holding_to_be_updated.shares
       
       current_shares = previous_shares_owned + shares_to_be_added
-        # p Holding.find(holding_id_to_be_updated)
+
         holding_to_be_updated.update_attribute(:shares, current_shares)
     end 
 
@@ -122,7 +115,7 @@ class TransactionsController < ApplicationController
       @transaction = Transaction.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def transaction_params
       params.require(:transaction).permit(:user_id, :buy_or_sell, :stock_symbol, :shares, :price_per_share)
     end
